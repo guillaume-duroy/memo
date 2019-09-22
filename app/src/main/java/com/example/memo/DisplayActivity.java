4b365bae5e83ec5cda5d61fb634581644b4a94ca
Guillaume <guillaume.du.roy@gmail.com>
2019-09-22 17:52:49 +0100

    Addition of second Activity

---
+++ app/src/main/java/com/example/memo/DisplayActivity.java
@@ -0,0 +1,31 @@
+package com.example.memo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("Message");
+        int viewType = intent.getIntExtra(MainActivity.EXTRA_MESSAGE, 0);
+
+        if (viewType == 1)
+        {
+            setContentView(R.layout.activity_display);
+        }
+
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(message);
+    }
+}
